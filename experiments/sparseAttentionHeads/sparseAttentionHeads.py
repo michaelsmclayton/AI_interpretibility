b8ab682c1c59cd6a45927c214dc34e85d5173f9a
michaelsmclayton <michaelsmclayton@googlemail.com>
2024-08-13 10:06:50 +0100

    Fix hook functions to allow training of sparse masks. Now running across all task types

--- experiments/sparseAttentionHeads/sparseAttentionHeads.py
+++ experiments/sparseAttentionHeads/sparseAttentionHeads.py
@@ -27,7 +27,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 # )
 
 # Define task parameters
-n_iterations_per_batch = 25
+n_iterations_per_batch = 5
 n_words_per_prompt = 4
 n_words_per_category = 10
 n_tokens = 50257
@@ -65,116 +65,121 @@ def getLogitDifference(cur_logits, prev_logits):
 
 # We define a head ablation hook
 def head_ablation_hook(value, hook, layer_idx, corrupt_activations):
-    with torch.no_grad():
-        for head_index in range(n_heads):
-            # Get current attention mask value (0 - 1)
-            cur_attention_mask = attention_head_mask[layer_idx, head_index]
-            cur_attention_v = value[:, :, head_index, :]
-            corrupt_attention_v = corrupt_activations[:, :, head_index, :]
-            valid_activation_v = cur_attention_v * cur_attention_mask
-            corrupt_activation_v = corrupt_attention_v * (1-cur_attention_mask)
-            value[:, :, head_index, :] = valid_activation_v + corrupt_activation_v
-    return value
+    new_value = value.clone()
+    for head_index in range(n_heads):
+        # Get current attention mask value (0 - 1)
+        cur_attention_mask = attention_head_mask[layer_idx, head_index]
+        cur_attention_v = value[:, :, head_index, :]
+        corrupt_attention_v = corrupt_activations[:, :, head_index, :]
+        valid_activation_v = cur_attention_v * cur_attention_mask
+        corrupt_activation_v = corrupt_attention_v * (1-cur_attention_mask)
+        new_value[:, :, head_index, :] = valid_activation_v + corrupt_activation_v
+    return new_value
 
 # -------------------------------------------------------
 # Train model
 # -------------------------------------------------------
 
-# Initialise attention masks
-attention_head_mask = torch.ones(model.cfg.n_layers, model.cfg.n_heads).to(device)
-
-# Define optimizer
-attention_head_mask.requires_grad = True
-optimizer = torch.optim.Adam([attention_head_mask], lr=0.01)
-
-# Define weight of accuracy and sparsity losses
-logit_difference_loss_weight = 1
-sparsity_loss_weight = .001
-
-# Define number of epochs
-n_epochs = 500
-
-# Print loss every n epochs
+# Training parameters
+n_epochs = 100
 n_print = 1
+logit_difference_loss_weight = 1
+sparsity_loss_weight = 1
 
 # Define n_words_per_prompt and n_words_per_category
-n_w_p = 1
-n_w_c = 10
-
-# Define best loss
-best_loss = np.inf
-best_attention_head_mask = None
-
-# Train model
-losses = []
-for i in range(n_epochs):
-    
-    # Reset all hooks
-    model.reset_hooks()
-    
-    # Apply contraints to attention head mask
-    with torch.no_grad():
-        attention_head_mask.data = torch.clamp(attention_head_mask, 0, 1)
-    
-    # Zero gradients
-    optimizer.zero_grad()
-    
-    # Initialise store for losses
-    cur_losses = []
-    
-    # Get task examples
-    valid, corrupt, correct_tokens = getTask(n_iterations_per_batch, n_w_p, n_w_c)
-    correct_tokens = torch.tensor(correct_tokens).to(device)
-    
-    # Run model on corrupted prompts
-    corrupt_tokens = model.to_tokens(corrupt, prepend_bos=True, padding_side="left")
-    corrupt_logits, corrupt_activations = model.run_with_cache(corrupt_tokens, remove_batch_dim=False)
-
-    # Run model on valid prompts
-    valid_tokens = model.to_tokens(valid, prepend_bos=True, padding_side="left")
-    valid_logits, _ = model.run_with_cache(valid_tokens, remove_batch_dim=False)
-
-    # Generate hooks across all layers
-    for layer_idx in range(n_layers):
-        temp_hook_fn = partial(head_ablation_hook, layer_idx=layer_idx, corrupt_activations=corrupt_activations[f"blocks.{layer_idx}.attn.hook_v"])
-        cur_activation_name = utils.get_act_name("v", layer_idx, "attn")
-        model.add_hook(cur_activation_name, temp_hook_fn)
-    
-    # Run model on valid prompts (with hooks)
-    valid_tokens = model.to_tokens(valid, prepend_bos=True, padding_side="left")
-    valid_logits_hk, valid_activations = model.run_with_cache(valid_tokens, remove_batch_dim=False)
-    
-    # Get loss
-    logit_difference_loss = getLogitDifference(valid_logits_hk[:,-1,:], valid_logits[:,-1,:])
+for n_w_p in range(1,n_words_per_prompt+1):
+    for n_w_c in range(1,n_words_per_category+1):
+        print(f"Training for {n_w_p} words per prompt and {n_w_c} words per category")
         
-    # Get task accuracy
-    answers = torch.argmax(valid_logits_hk[:,-1,:], dim=-1)
-    accuracy_loss = torch.mean((answers == correct_tokens).float()) * logit_difference_loss_weight
-    
-    # Get sparsity loss
-    sparsity_loss = torch.norm(attention_head_mask, p=1)
-    sparsity_loss = sparsity_loss / np.prod(attention_head_mask.shape)
-    sparsity_loss *= sparsity_loss_weight
+        # Check if attention mask already exists
+        if os.path.exists(f"./savedSparsityMasks/{n_w_p}_{n_w_c}_attention_mask_{n_epochs}epochs_{n_iterations_per_batch}examples.pt"):
+            continue
 
-    # Get total loss and backpropagate    
-    loss = logit_difference_loss + sparsity_loss
-    loss.backward()
-    optimizer.step()
-    losses.append([logit_difference_loss.item(), sparsity_loss.item()])
-    
-    # Save current sparse attention head mask (if loss is better)
-    if loss.item() < best_loss:
-        best_loss = loss.item()
-        best_attention_head_mask = attention_head_mask.detach().clone()
-    
-    # Print loss
-    if i % n_print == 0:
-        print(f"Epoch {i}: Logit difference loss: {logit_difference_loss.item()}, Sparsity Loss: {sparsity_loss.item()}, (Accuracy = {accuracy_loss.item()})")
+        # Initialise attention masks
+        attention_head_mask = 1 * torch.ones(model.cfg.n_layers, model.cfg.n_heads).to(device)
 
-attention_head_mask_np = attention_head_mask.detach().cpu().numpy()
+        # Define optimizer
+        attention_head_mask.requires_grad = True
+        optimizer = torch.optim.Adam([attention_head_mask], lr=0.05)
 
-best_attention_head_mask_np = best_attention_head_mask.detach().cpu().numpy()
+        # Define best loss
+        best_loss = np.inf
+        best_attention_head_mask = None
 
-plt.close()
-plt.imshow(best_attention_head_mask_np, cmap="viridis")
-plt.savefig("attention_head_mask.png")
+        # Train model
+        losses = []
+        for i in range(n_epochs):
+            
+            # Reset all hooks
+            model.reset_hooks()
+            
+            # Get task examples
+            valid, corrupt, correct_tokens = getTask(n_iterations_per_batch, n_w_p, n_w_c)
+            correct_tokens = torch.tensor(correct_tokens).to(device)
+            
+            # Run model on corrupted prompts
+            corrupt_tokens = model.to_tokens(corrupt, prepend_bos=True, padding_side="left")
+            corrupt_logits, corrupt_activations = model.run_with_cache(corrupt_tokens, remove_batch_dim=False)
+
+            # Run model on valid prompts
+            valid_tokens = model.to_tokens(valid, prepend_bos=True, padding_side="left")
+            valid_logits, _ = model.run_with_cache(valid_tokens, remove_batch_dim=False)
+
+            # Generate hooks across all layers
+            for layer_idx in range(n_layers):
+                temp_hook_fn = partial(head_ablation_hook, layer_idx=layer_idx, corrupt_activations=corrupt_activations[f"blocks.{layer_idx}.attn.hook_v"])
+                cur_activation_name = utils.get_act_name("v", layer_idx, "attn")
+                model.add_hook(cur_activation_name, temp_hook_fn)
+            
+            # Run model on valid prompts (with hooks)
+            valid_tokens = model.to_tokens(valid, prepend_bos=True, padding_side="left")
+            valid_logits_hk, valid_activations = model.run_with_cache(valid_tokens, remove_batch_dim=False)
+            
+            # Get loss
+            logit_difference_loss = getLogitDifference(valid_logits_hk[:,-1,:], valid_logits[:,-1,:]) * logit_difference_loss_weight
+                
+            # Get task accuracy
+            answers = torch.argmax(valid_logits_hk[:,-1,:], dim=-1)
+            accuracy_loss = torch.mean((answers == correct_tokens).float())
+            
+            # Get sparsity loss
+            sparsity_loss = torch.norm(attention_head_mask, p=1)
+            sparsity_loss = sparsity_loss / np.prod(attention_head_mask.shape)
+            sparsity_loss *= sparsity_loss_weight
+
+            # Get total loss and backpropagate    
+            optimizer.zero_grad()
+            loss = logit_difference_loss + sparsity_loss
+            loss.backward()
+            optimizer.step()
+            losses.append([logit_difference_loss.item(), sparsity_loss.item()])
+            
+            # Save current sparse attention head mask (if loss is better)
+            if loss.item() < best_loss:
+                best_loss = loss.item()
+                best_attention_head_mask = attention_head_mask.detach().clone()
+            
+            # Print loss
+            if i % n_print == 0:
+                print(f"Epoch {i}: Logit difference loss: {logit_difference_loss.item()}, Sparsity Loss: {sparsity_loss.item()}, (Accuracy = {accuracy_loss.item()})")
+
+        # Get remaining attention head patterns
+        attention_head_mask_np = attention_head_mask.detach().cpu().numpy()
+        best_attention_head_mask_np = best_attention_head_mask.detach().cpu().numpy()
+        
+        # Save attention masks
+        torch.save(attention_head_mask, f"./savedSparsityMasks/{n_w_p}_{n_w_c}_attention_mask_{n_epochs}epochs_{n_iterations_per_batch}examples.pt")
+        torch.save(best_attention_head_mask_np, f"./savedSparsityMasks/{n_w_p}_{n_w_c}_best_attention_mask_{n_epochs}epochs_{n_iterations_per_batch}examples.pt")
+
+# # Plot losses
+# plt.close()
+# plt.plot(losses)
+# plt.savefig("losses.png")
+
+# # Plot
+# plt.close()
+# fig,ax = plt.subplots(1,2,figsize=(10,5))
+# ax[0].hist(attention_head_mask_np.flatten(), bins=100)
+# ax[1].imshow(attention_head_mask_np, cmap="viridis")
+# # plt.colorbar(ax=ax[1])
+# plt.savefig("attention_head_mask.png")
